3691087cb9224a9161247ebf0c75ae2a7f74bfbd
wl020814 <1833446598@qq.com>
2019-07-02 10:17:19 +0800

    0702.1016 draw migong

---
+++ vi_gong.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define Row 6
+#define Column 6
+int g_miGong[Row][Column] = {0};
+
+
+struct Pos {
+    int x;
+    int y;
+}
+
+struct Stack {
+    Pos pos;
+    Stack *stack;
+}
+
+void PushStack(Pos pos)
+{
+
+}
+
+void NewAMap(void)
+{
+    int i, j;
+    for (i = 0; i < Row; i++) {
+        printf("please input %d nums to draw map row %d.\n", Column ,i);
+        for (j = 0; j < Column; j++) {
+            scanf("%d", &g_miGong[i][j]);
+        }
+    }
+}
+
+void DrawTheMap(void)
+{
+    int i, j;
+    for (i = 0; i < Row; i++) {
+        for (j = 0; j< Column; j++) {
+            printf("%d ", g_miGong[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    NewAMap();
+    DrawTheMap();
+    return 0;
+}
